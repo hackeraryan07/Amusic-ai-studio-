d96c2b277828c27e6ee1b8bf196b709426c956b1
hackeraryan07 <hackeraryan07@gmail.com>
2026-06-18 12:29:53 +0530

    Updated the configuration to update app not conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,11 +11,12 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
+    // Unique applicationId for the applet. Increased versionCode to 2 to support clean updates over previous installations without conflicts.
     applicationId = "com.aistudio.amusic.lqrvwz"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
